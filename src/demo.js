81cc9898017293744bb2f2a0a3230b12b5003fa3
vjspranav <pranavasri@live.in>
2021-01-04 01:13:20 +0530

    Adding a state check box * Todo Update table value

--- src/demo.js
+++ src/demo.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState, useReducer } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
@@ -7,6 +7,19 @@ import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
+
+const Checkbox = ({ fnIct, title = "", checked = false }) => (
+  <label>
+    <input
+      onClick={(e) => {
+        if (fnIct !== undefined) fnIct(e.target.checked);
+      }}
+      type="checkbox"
+      checked={checked}
+    />
+    {title}
+  </label>
+);
 
 const useStyles = makeStyles({
   table: {
@@ -78,47 +91,76 @@ const rows = [
 export default function BasicTable() {
   const classes = useStyles();
 
+  const initialState = {
+    ict: true,
+  };
+
+  const reducer = (state, action) => ({ ...state, ...action });
+  const [state, setState] = useReducer(reducer, initialState);
+
+  const clearFilter = () => setState(initialState);
   const [sem, setSem] = useState(0);
 
   return (
     <>
-    <div>
-      Select sem:
-      <select value={sem} onChange={(event)=>{setSem(event.target.value)}}>
-        <option value={0}>H1</option>
-        <option value={1}>H2</option>
-      </select>
-    </div>
-    <TableContainer component={Paper}>
-      <Table className={classes.table} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Days / Time</TableCell>
-            <TableCell align="center">9 - 10:30</TableCell>
-            <TableCell align="center">10:30 - 12:00</TableCell>
-            <TableCell align="center">12:00 - 13:30</TableCell>
-            <TableCell align="center">14:00 - 15:30</TableCell>
-            <TableCell align="center">15:30 - 1700</TableCell>
-            <TableCell align="center">17:00 - 18:30</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.day}>
-              <TableCell component="th" scope="row">
-                {row.day}
-              </TableCell>
-              <TableCell align="center">{row.s1[sem]}</TableCell>
-              <TableCell align="center">{row.s2[sem]}</TableCell>
-              <TableCell align="center">{row.s3[sem]}</TableCell>
-              <TableCell align="center">{row.s4[sem]}</TableCell>
-              <TableCell align="center">{row.s5[sem]}</TableCell>
-              <TableCell align="center">{row.s6[sem]}</TableCell>
+      <TableContainer component={Paper}>
+        <Table className={classes.table} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Days / Time</TableCell>
+              <TableCell align="center">9 - 10:30</TableCell>
+              <TableCell align="center">10:30 - 12:00</TableCell>
+              <TableCell align="center">12:00 - 13:30</TableCell>
+              <TableCell align="center">14:00 - 15:30</TableCell>
+              <TableCell align="center">15:30 - 1700</TableCell>
+              <TableCell align="center">17:00 - 18:30</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow key={row.day}>
+                <TableCell component="th" scope="row">
+                  {row.day}
+                </TableCell>
+                <TableCell align="center">{row.s1[sem]}</TableCell>
+                <TableCell align="center">{row.s2[sem]}</TableCell>
+                <TableCell align="center">{row.s3[sem]}</TableCell>
+                <TableCell align="center">{row.s4[sem]}</TableCell>
+                <TableCell align="center">{row.s5[sem]}</TableCell>
+                <TableCell align="center">{row.s6[sem]}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <div>
+        Select sem:
+        <select
+          value={sem}
+          onChange={(event) => {
+            setSem(event.target.value);
+            ict_bool = false;
+          }}
+        >
+          <option value={0}>H1</option>
+          <option value={1}>H2</option>
+        </select>
+        <div className="App">
+          <button onClick={() => clearFilter()}>Defaults</button>
+          <br />
+          <Checkbox
+            title="ICT"
+            fnIct={(v) =>
+              setState({
+                ict: v,
+              })
+            }
+            checked={state.ict}
+          />
+          <br />
+          ICT: {state.ict ? "true" : "false"}
+        </div>
+      </div>
     </>
   );
 }
