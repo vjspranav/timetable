383ac946d427d1a8c48e56a1e08fc5c079f7b390
vjspranav <pranavasri@live.in>
2021-11-05 04:50:03 +0530

    Trying vertical view for mobile

--- src/timetable.js
+++ src/timetable.js
@@ -29,6 +29,21 @@ import Button from "@mui/material/Button";
 import FormGroup from "@mui/material/FormGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Checkbox from "@mui/material/Checkbox";
+import PropTypes from "prop-types";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import { useMediaQuery } from "react-responsive";
+
+// Map 1-7 to Monday to Sunday
+const days = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
 
 function clearAllPreferences() {
   localStorage.removeItem("checkboxValues");
@@ -289,81 +304,243 @@ export default function BasicTable() {
       </Modal>
     );
   };
+  const HorizontalTimeTable = () => {
+    return (
+      <TableContainer component={Paper}>
+        <Table className={classes.table} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Days / Time</TableCell>
+              <TableCell align="center">09:00 - 10:30</TableCell>
+              <TableCell align="center">10:30 - 12:00</TableCell>
+              <TableCell align="center">12:00 - 13:30</TableCell>
+              <TableCell align="center">14:00 - 15:30</TableCell>
+              <TableCell align="center">15:30 - 17:00</TableCell>
+              <TableCell align="center">17:00 - 18:30</TableCell>
+              <TableCell align="center">18:30 - 19:00</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {Object.keys(timetable).map((day) => (
+              <StyledTableRow key={day}>
+                <TableCell component="th" scope="row">
+                  {day}
+                </TableCell>
+                {Object.keys(timetable[day]).map((period) => {
+                  return (
+                    <TableCell align="center" key={period}>
+                      {timetable[day][period].map((item) => {
+                        let curCourse = courses.find(
+                          (course) => course.id == item.id && course.selected
+                        );
+                        let tmp = null;
+                        if (curCourse) {
+                          if (curCourse.type != "common") {
+                            if (sem != "all") {
+                              if (curCourse.type == sem) {
+                                tmp = `${
+                                  curCourse.name
+                                } (${sem.toUpperCase()})`;
+                              }
+                            } else {
+                              tmp = `${
+                                curCourse.name
+                              } (${curCourse.type.toUpperCase()})`;
+                            }
+                          } else {
+                            tmp = curCourse.name;
+                          }
+                          if (item.tut && tmp) {
+                            tmp = `${tmp} - Tutorial`;
+                          }
+                        }
+                        return tmp ? (
+                          <div key={tmp}>
+                            <Chip
+                              id={tmp}
+                              label={tmp}
+                              className={classes.chip}
+                              color={item.tut ? "secondary" : "primary"}
+                            />
+                          </div>
+                        ) : null;
+                      })}
+                    </TableCell>
+                  );
+                })}
+              </StyledTableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    );
+  };
+  const TableCellWithdata = ({ index, period }) => {
+    return (
+      <TableCell>
+        {timetable[days[index]][period].map((item) => {
+          let curCourse = courses.find(
+            (course) => course.id == item.id && course.selected
+          );
+          let tmp = null;
+          if (curCourse) {
+            if (curCourse.type != "common") {
+              if (sem != "all") {
+                if (curCourse.type == sem) {
+                  tmp = `${curCourse.name} (${sem.toUpperCase()})`;
+                }
+              } else {
+                tmp = `${curCourse.name} (${curCourse.type.toUpperCase()})`;
+              }
+            } else {
+              tmp = curCourse.name;
+            }
+            if (item.tut && tmp) {
+              tmp = `${tmp} - Tutorial`;
+            }
+          }
+          return tmp ? (
+            <div key={tmp}>
+              <Chip
+                id={tmp}
+                label={tmp}
+                className={classes.chip}
+                color={item.tut ? "secondary" : "primary"}
+              />
+            </div>
+          ) : null;
+        })}
+      </TableCell>
+    );
+  };
+  const VerticalTimeTable = () => {
+    function TabPanel(props) {
+      const { children, value, index, ...other } = props;
 
+      return (
+        <div
+          role="tabpanel"
+          hidden={value !== index}
+          id={`vertical-tabpanel-${index}`}
+          aria-labelledby={`vertical-tab-${index}`}
+          style={{ width: "100%" }}
+          {...other}
+        >
+          <Table>
+            <TableBody>
+              <TableRow>
+                <TableCell align="center" variant="head">
+                  Time
+                </TableCell>
+                <TableCell variant="head">Course</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell align="center">09:00 - 10:30</TableCell>
+                <TableCellWithdata index={index} period="period1" />
+              </TableRow>
+              <TableRow>
+                <TableCell align="center">10:30 - 12:00</TableCell>
+                <TableCellWithdata index={index} period="period2" />
+              </TableRow>
+              <TableRow>
+                <TableCell align="center">12:00 - 13:30</TableCell>
+                <TableCellWithdata index={index} period="period3" />
+              </TableRow>
+              <TableRow>
+                <TableCell align="center">14:00 - 15:30</TableCell>
+                <TableCellWithdata index={index} period="period4" />
+              </TableRow>
+              <TableRow>
+                <TableCell align="center">15:30 - 17:00</TableCell>
+                <TableCellWithdata index={index} period="period5" />
+              </TableRow>
+              <TableRow>
+                <TableCell align="center">17:00 - 18:30</TableCell>
+                <TableCellWithdata index={index} period="period6" />
+              </TableRow>
+              <TableRow>
+                <TableCell align="center">18:30 - 19:00</TableCell>
+                <TableCellWithdata index={index} period="period7" />
+              </TableRow>
+            </TableBody>
+          </Table>
+        </div>
+      );
+    }
+    TabPanel.propTypes = {
+      children: PropTypes.node,
+      index: PropTypes.number.isRequired,
+      value: PropTypes.number.isRequired,
+    };
+    function a11yProps(index) {
+      return {
+        id: `vertical-tab-${index}`,
+        "aria-controls": `vertical-tabpanel-${index}`,
+      };
+    }
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event, newValue) => {
+      setValue(newValue);
+    };
+
+    return (
+      <Box
+        sx={{
+          flexGrow: 1,
+          bgcolor: "background.paper",
+          display: "flex",
+          height: "80%",
+        }}
+      >
+        <Tabs
+          orientation="vertical"
+          value={value}
+          variant="scrollable"
+          onChange={handleChange}
+          aria-label="Vertical tabs example"
+          sx={{ borderRight: 1, borderColor: "divider" }}
+        >
+          <Tab label="Monday" {...a11yProps(0)} />
+          <Tab label="Tuesday" {...a11yProps(1)} />
+          <Tab label="Wednesday" {...a11yProps(2)} />
+          <Tab label="Thursday" {...a11yProps(3)} />
+          <Tab label="Friday" {...a11yProps(4)} />
+          <Tab label="Saturday" {...a11yProps(5)} />
+        </Tabs>
+        <TabPanel value={value} index={0}>
+          Item One
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          Item Two
+        </TabPanel>
+        <TabPanel value={value} index={2}>
+          Item Three
+        </TabPanel>
+        <TabPanel value={value} index={3}>
+          Item Four
+        </TabPanel>
+        <TabPanel value={value} index={4}>
+          Item Five
+        </TabPanel>
+        <TabPanel value={value} index={5}>
+          Item Six
+        </TabPanel>
+      </Box>
+    );
+  };
+
+  const isDesktopOrLaptop = useMediaQuery({
+    query: "(min-width: 1224px)",
+  });
+  const isBigScreen = useMediaQuery({ query: "(min-width: 1824px)" });
+  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 1224px)" });
   return (
     <div>
       <Topbar sem={sem} setSem={setSem} handleOpen={handleOpen} />
       <div>
         <CustomModal />
         <>
-          <TableContainer component={Paper}>
-            <Table className={classes.table} aria-label="simple table">
-              <TableHead>
-                <TableRow>
-                  <TableCell>Days / Time</TableCell>
-                  <TableCell align="center">09:00 - 10:30</TableCell>
-                  <TableCell align="center">10:30 - 12:00</TableCell>
-                  <TableCell align="center">12:00 - 13:30</TableCell>
-                  <TableCell align="center">14:00 - 15:30</TableCell>
-                  <TableCell align="center">15:30 - 17:00</TableCell>
-                  <TableCell align="center">17:00 - 18:30</TableCell>
-                  <TableCell align="center">18:30 - 19:00</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {Object.keys(timetable).map((day) => (
-                  <StyledTableRow key={day}>
-                    <TableCell component="th" scope="row">
-                      {day}
-                    </TableCell>
-                    {Object.keys(timetable[day]).map((period) => {
-                      return (
-                        <TableCell align="center" key={period}>
-                          {timetable[day][period].map((item) => {
-                            let curCourse = courses.find(
-                              (course) =>
-                                course.id == item.id && course.selected
-                            );
-                            let tmp = null;
-                            if (curCourse) {
-                              if (curCourse.type != "common") {
-                                if (sem != "all") {
-                                  if (curCourse.type == sem) {
-                                    tmp = `${
-                                      curCourse.name
-                                    } (${sem.toUpperCase()})`;
-                                  }
-                                } else {
-                                  tmp = `${
-                                    curCourse.name
-                                  } (${curCourse.type.toUpperCase()})`;
-                                }
-                              } else {
-                                tmp = curCourse.name;
-                              }
-                              if (item.tut && tmp) {
-                                tmp = `${tmp} - Tutorial`;
-                              }
-                            }
-                            return tmp ? (
-                              <div key={tmp}>
-                                <Chip
-                                  id={tmp}
-                                  label={tmp}
-                                  className={classes.chip}
-                                  color={item.tut ? "secondary" : "primary"}
-                                />
-                              </div>
-                            ) : null;
-                          })}
-                        </TableCell>
-                      );
-                    })}
-                  </StyledTableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
           <div>
             {/* <select
           value={sem}
@@ -379,6 +556,9 @@ export default function BasicTable() {
           <option value={"h1"}>H1</option>
           <option value={"h2"}>H2</option>
         </select>{" "} */}
+            {/* <VerticalTimeTable /> */}
+            {isDesktopOrLaptop && <HorizontalTimeTable />}
+            {isTabletOrMobile && <VerticalTimeTable />}
             <div
               style={{
                 marginTop: "15px",
